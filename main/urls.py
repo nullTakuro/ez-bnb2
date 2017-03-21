a8335e0593d32e3eebe534ba8cb2a8deca19c8d2
nullTakuro <f-op@hotmail.co.uk>
2017-03-21 14:59:48 +0000

    removed all static urls and replaced all with dynamic ones

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     #/users/
     url(r'^users/$', views.users, name='users'),   #default home page for the login page
-    #/bookings/id="corresponding ID"
+    #/booking/id="corresponding ID"
     url(r'^booking/id=(?P<booking_id>[0-9]+)/$', views.bookingRender, name='bookingRender'),
     #/user/id="corresponding ID"
     url(r'^user/id=(?P<user_id>[0-9]+)/$', views.userRender, name='userRender'),
