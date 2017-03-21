7b763a174de378fb7d031a57b42aaa2ba01a3ddd
nullTakuro <f-op@hotmail.co.uk>
2017-03-21 13:36:04 +0000

    These two lines are the cause of your troubles. They aren't returning an iterable, such as a list, but an object, which isn't iterable at all. Instead of a get, use a filter instead, which will return a list.

--- main/views.py
+++ main/views.py
@@ -9,7 +9,7 @@ def index(request):
     return render(request, 'main/index.html', {'all_users' : all_users}  )
 
 def userRender(request, user_id):
-    all_bookings = booking.objects.filter(pk=user_id)
+    all_bookings = booking.objects.get(pk=user_id) #These two lines are the cause of your troubles. They aren't returning an iterable, such as a list, but an object, which isn't iterable at all. Instead of a get, use a filter instead, which will return a list.
     return render(request, 'main/user.html', {'all_bookings' : all_bookings})
 
 def bookingRender(request, booking_id):
